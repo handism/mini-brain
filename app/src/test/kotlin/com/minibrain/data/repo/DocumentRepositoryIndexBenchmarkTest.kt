84069f8a683ee6f93390e52bc76321cd3bcc5aa4
handism <andy2roidy@gmail.com>
2026-07-30 20:06:55 +0900

    🧹 Remove unused import in DocumentRepositoryIndexBenchmarkTest

--- app/src/test/kotlin/com/minibrain/data/repo/DocumentRepositoryIndexBenchmarkTest.kt
+++ app/src/test/kotlin/com/minibrain/data/repo/DocumentRepositoryIndexBenchmarkTest.kt
@@ -20,7 +20,6 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
-import kotlin.system.measureTimeMillis
 
 @RunWith(RobolectricTestRunner::class)
 @Config(sdk = [33])
